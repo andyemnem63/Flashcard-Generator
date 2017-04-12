53352ef58e9a965f450f5033da053b92be6541f8
AndyEm <andyem64@gmail.com>
2017-04-12 00:34:07 -0400

    complete cloze logic

--- cloze.js
+++ cloze.js
@@ -1,6 +1,16 @@
-var cloze = function(text,cloze) {
+var cloze = function(text , cloze) {
 		this.text = text;
 		this.cloze = cloze;
+		this.partial = this.text.replace(this.cloze, '...........');
 }
+
+
+// var thirdPresident = new cloze("George Washington was the third president of the U.S" , 'Thomas Jefferson');
+
+// console.log('partial:' , thirdPresident.partial);
+
+// console.log('cloze:' , thirdPresident.cloze);
+
+// console.log('full_text:' , thirdPresident.text);
 
 module.exports = cloze;
--- main.js
+++ main.js
@@ -6,12 +6,22 @@ var clozeCard = require('./cloze');
 var count = 0;
 var correctCount = 0;
 
+//=========Basic===========================
 // Create in instance of function basic(){} from card.js and assign it to basic quest
 var firstPresident = new basicCard('Who was the first president of the U.S', 'George Washington');
 var secondPresident = new basicCard('Who was the second president of the U.S', 'John Adams');
-
+//Holds questions
 var basicQuestion = [firstPresident.front , secondPresident.front];
+//Holds answers
 var basicAnswer = [firstPresident.back , secondPresident.back];
+
+//=========Cloze==============
+var thirdPresident = new clozeCard("Thomas Jefferson was the third president of the U.S" , 'Thomas Jefferson');
+var fourthPresident = new clozeCard("James Madison was the forth president of the U.S" , 'James Madison');
+//Holds questions
+var clozeQuestion = [thirdPresident.partial , fourthPresident.partial];
+//Holds answers
+var clozeAnswer = [thirdPresident.cloze , fourthPresident.cloze];
 // Ask user if they want to play game with BASIC or CLOZE flashcards
 inquirer.prompt([
 
@@ -29,13 +39,13 @@ inquirer.prompt([
 		}
 		// If users answer is cloze.......
 		else if(answer.choice === 'Cloze'){
-			console.log('cloze');
+			cloze();
 		}
 })
 // Ask questions for basic flashCards. Using recursion to repeat
 function basic() {
-	if(count < 2){
-		
+
+	if(count < basicAnswer.length){
 		inquirer.prompt([
 			{
 				type:'input',
@@ -48,18 +58,42 @@ function basic() {
 			//Checks if users answer was correct
 			if(answer.input === basicAnswer[count]){
 				correctCount++;
-				console.log(true);
-			}
-			else{
-				console.log(false);
 			}
 			count++;
+			//Calls function to continue to the next question 
 			basic();
 		})
 	}
+	//Dispalys end of game results
 	else{
 		console.log('You got' ,correctCount, 'Right');
 	}
-
 }
+
+function cloze() {
+
+	if(count < clozeQuestion.length) {
+		inquirer.prompt([
+				{
+					type:'input',
+					name: 'input',
+					message: clozeQuestion[count]
+				}
+
+		])
+		.then(function(answer){
+			if(clozeAnswer[count] === answer.input) {
+				correctCount++;
+			}
+				count++
+				cloze();
+		});
+
+	}
+	else {
+		console.log('You got' ,correctCount, 'Right');
+	}
+}
+
+
 
